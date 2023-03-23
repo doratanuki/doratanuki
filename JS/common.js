bd5fd0075d8a861a3ab07fe22ed9900eeecc47ea
doratanuki <128441152+doratanuki@users.noreply.github.com>
2023-03-23 23:30:22 +0900

    Add files via upload

--- JS/common.js
+++ JS/common.js
@@ -12,6 +12,6 @@ $(".header-menu-item a").click(function () {//ナビゲーションのリンク�
 //ローディング画面
 //logoの表示
 $(window).on('load',function(){
-    $("#splash").delay(1500).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
-    $("#splash_logo").delay(1200).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
+    $("#splash").delay(1300).fadeOut('slow');//ローディング画面を1.5秒（1500ms）待機してからフェードアウト
+    $("#splash_logo").delay(1000).fadeOut('slow');//ロゴを1.2秒（1200ms）待機してからフェードアウト
   });
