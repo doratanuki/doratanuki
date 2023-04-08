fa0967088bdb4cc61689ab2c4cf95040b74255bc
doratanuki <128441152+doratanuki@users.noreply.github.com>
2023-04-08 18:58:30 +0900

    Add files via upload

---
+++ html/works/project2/js/style.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.list-frame > .English-spelling').click(function(){
+        $(this).addClass("hide")
+        $(this).siblings(".Japanese-spelling").removeClass("hide")
+    });
+    $('.list-frame > .Japanese-spelling').click(function(){
+        $(this).addClass("hide")
+        $(this).siblings(".English-spelling").removeClass("hide")
+    });
+});
