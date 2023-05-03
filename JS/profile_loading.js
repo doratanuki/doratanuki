e444e315907ee63f4eab28e1cc963603c76245d2
doratanuki <128441152+doratanuki@users.noreply.github.com>
2023-05-03 17:50:28 +0900

    Add files via upload

--- JS/profile_loading.js
+++ JS/profile_loading.js
@@ -1,6 +1,5 @@
 //ローディング画面
-//logoの表示
 $(window).on('load',function(){
-    $("#splash").delay(2000).fadeOut('slow');
-    $("#video").delay(1700).fadeOut('slow');
-  });
+  $("#splash").delay(2000).fadeOut('slow');
+  $("#video").delay(1700).fadeOut('slow');
+});
